5302e025983438ccac9a1badc7a975384327122d
zhujinxuan <zhujinxuan@gmail.com>
2018-04-05 17:51:28 -0400

    Fix getData on td

--- src/style/getData.js
+++ src/style/getData.js
@@ -1,6 +1,6 @@
 // @flow
 
-const tagTextAlign = ['div', 'p', 'h1', 'h2', 'h3', 'ul', 'ol'];
+const tagTextAlign = ['div', 'p', 'h1', 'h2', 'h3', 'ul', 'ol', 'td'];
 const tagFloat = ['div', 'p', 'h1', 'h2', 'h3', 'img'];
 const tagNames = [...tagTextAlign, ...tagFloat];
 const alignments = ['left', 'center', 'right'];
@@ -9,15 +9,16 @@ function getData(el: Element): { textAlign?: string } {
     if (tagNames.indexOf(el.tagName.toLowerCase()) === -1) {
         return {};
     }
-    if (el.getAttribute('align')) {
-        const align: ?string = el.getAttribute('align');
+
+    if (el.style && typeof el.style.textAlign === 'string') {
+        const align: string = el.style.textAlign;
         if (align && alignments.indexOf(align) > -1) {
             return { textAlign: align };
         }
     }
 
-    if (el.style && typeof el.style.textAlign === 'string') {
-        const align: string = el.style.textAlign;
+    if (el.getAttribute('align')) {
+        const align: ?string = el.getAttribute('align');
         if (align && alignments.indexOf(align) > -1) {
             return { textAlign: align };
         }
